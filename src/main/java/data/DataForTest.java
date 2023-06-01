c1a2a88b25104aa36afcda0cedf4a3533be1b100
DmitriySlushkov <dimon.yalta@gmail.com>
2023-06-01 10:46:52 +0300

    Initial commit

---
+++ src/main/java/data/DataForTest.java
@@ -0,0 +1,24 @@
+package data;
+
+public class AsteroidData {
+    private String asteroidName;
+    private String asteroidId;
+
+    public AsteroidData(String asteroidName, String asteroidId) {
+        this.asteroidName = asteroidName;
+        this.asteroidId = asteroidId;
+    }
+
+    public String getAsteroidName() {
+        return asteroidName;
+    }
+
+    public String getAsteroidId() {
+        return asteroidId;
+    }
+
+
+
+
+
+}
