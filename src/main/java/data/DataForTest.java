9c567f7ba82887ac45f92c038d9a3fcd93514e17
DmitriySlushkov <dimon.yalta@gmail.com>
2023-06-01 10:52:18 +0300

    my commit

--- src/main/java/data/DataForTest.java
+++ src/main/java/data/DataForTest.java
@@ -1,12 +1,16 @@
 package data;
 
-public class AsteroidData {
-    private String asteroidName;
-    private String asteroidId;
+public class DataForTest {
+    private final String asteroidName = "Icarus";
+    private final String asteroidId = "2001566";
+    private final String API_KEY = "ZCfZmZffzHjlNxLPPU2go6Y7YdRR2Dt9QR1V9KCC";
 
-    public AsteroidData(String asteroidName, String asteroidId) {
-        this.asteroidName = asteroidName;
-        this.asteroidId = asteroidId;
+    public DataForTest(){
+
+    }
+
+    public String getAPI_KEY() {
+        return API_KEY;
     }
 
     public String getAsteroidName() {
--- src/test/java/TestPresenceOfAsteroidInTheList.java
+++ src/test/java/TestPresenceOfAsteroidInTheList.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class TestPresenceOfAsteroidInTheList {
+import data.DataForTest;
+import io.qameta.allure.junit4.DisplayName;
+import io.qameta.allure.restassured.AllureRestAssured;
+import io.restassured.RestAssured;
+import org.junit.Before;
+import org.junit.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.hasItems;
+
+
+public class TestPresenceOfAsteroidInTheList {
+    @Before
+    public void setUp(){
+        RestAssured.baseURI = "https://api.nasa.gov/neo/rest/v1/neo";
+    }
+
+
+    @Test
+    @DisplayName("Проверка API получения общего набора данных об астероидах и поиск конкретного астероида")
+    public void checkBrowseDataSetAndSearchAsteroid(){
+        DataForTest testData = new DataForTest();
+        given()
+                .filter(new AllureRestAssured())
+                .get("/browse?api_key="+testData.getAPI_KEY())
+                .then()
+                .log().all()
+                .assertThat()
+                .statusCode(200)
+                .and()
+                .body(("near_earth_objects.id"), hasItems(testData.getAsteroidId()))
+                .and()
+                .body(("near_earth_objects.name_limited"), hasItems(testData.getAsteroidName()));
+    }
+
 }
